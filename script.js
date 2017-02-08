788f3323e36be70d0bc90bec0d775bc09f92ef4f
KarolinaHetmanska <khetmanska30@wp.pl>
2017-02-08 20:44:35 +0100

    WIP (try to display img preview)

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-$(function(){
-  $('div').click(function () {
-    $('div').hide()
-  })
 
+$(function(){
+  var $input = $('input:file')
+  $input.change(function (e) {
+  // var $inputValue = $input.val();
+    var fileName = e.target.files[0].name;
+    $('.display-files').append('<img src="'+fileName+'"/>')
+   // alert('The file "' + fileName +  '" has been selected.');
+   //console.log("input:file before", $inputValue);
+  //  ($inputValue.substring(3,11)=== "fakepath")? $inputValue = $inputValue.substring((12)) : $inputValue
+  //  console.log("input:file after substring", $inputValue);
+
+
+ // $('.display-files').html($inputValue);
+  })
 })
