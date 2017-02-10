31d218b72df89a8d135b302249d5d51eb0ccbda4
KarolinaHetmanska <khetmanska30@wp.pl>
2017-02-10 20:16:47 +0100

    WIP, Change image label - trying make it works

--- script.js
+++ script.js
@@ -1,29 +1,37 @@
 $(function () {
+
   $('#add-file').change(function () {
     var $files = $('input:file')[0].files
+
     //  console.log("input", $files)
+    for (var i = 0; i < $files.length; i += 1) {
+      function readAndPreview(file) {
+        var reader = new FileReader()
+        //  console.log('reader', reader)
 
-    function readAndPreview(file) {
-      var reader = new FileReader()
-      //  console.log('reader', reader)
-
-      reader.onload = function () {
-        var image = new Image()
-        image.src = this.result
-        $('.preview').append('<div class="single-image-preview"><img src="' + image.src + '"/><div class="change-image">Zmień zdjęcie</div><div class="remove-image">Usuń zdjęcie</div></div>')
-        //   console.log("image", image)
-        //   console.log("this", this)
+        reader.onload = function () {
+          var image = new Image()
+          image.src = this.result
+          $('.preview').append('<div class="single-image-preview"><img src="' + image.src + '"/><label for="add-file" class="change-image">Zmień zdjęcie</label><div class="remove-image">Usuń zdjęcie</div></div>')
+          //   console.log("image", image)
+          //   console.log("this", this)
+        }
+        reader.readAsDataURL(file)
       }
-      reader.readAsDataURL(file)
-    }
 
-    if ($files) {
-      [].forEach.call($files, readAndPreview)
+      readAndPreview($files[i])
     }
   })
+
   $(document).on('click', '.remove-image', function () {
     $(this).parent().remove()
-    console.log(this)
+    // console.log(this)
   })
+  $(document).on('click', '.change-image', function () {
+      $(this).siblings('img').remove()
+      console.log('this change', this)
+
+    }
+  )
 
 })
