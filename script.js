0b1073575b979e6cbfb157e77c12a24fb7b13633
KarolinaHetmanska <khetmanska30@wp.pl>
2017-02-10 16:51:18 +0100

    Remove picture is working

--- script.js
+++ script.js
@@ -1,30 +1,29 @@
 $(function () {
   $('#add-file').change(function () {
     var $files = $('input:file')[0].files
-    console.log("input", $files)
+    //  console.log("input", $files)
 
     function readAndPreview(file) {
       var reader = new FileReader()
-      console.log('reader', reader)
+      //  console.log('reader', reader)
 
       reader.onload = function () {
         var image = new Image()
         image.src = this.result
         $('.preview').append('<div class="single-image-preview"><img src="' + image.src + '"/><div class="change-image">Zmień zdjęcie</div><div class="remove-image">Usuń zdjęcie</div></div>')
-        console.log("image", image)
-        console.log("this", this)
+        //   console.log("image", image)
+        //   console.log("this", this)
       }
-
-
       reader.readAsDataURL(file)
-
     }
 
     if ($files) {
       [].forEach.call($files, readAndPreview)
-
     }
-
+  })
+  $(document).on('click', '.remove-image', function () {
+    $(this).parent().remove()
+    console.log(this)
   })
 
 })
