dbfb96097f00a8a48c381ffce86821bb567f970e
Karolina Hetmanska <khetmanska@k85.pl>
2017-02-13 21:54:11 +0100

    Change image works, (without styling)

--- script.js
+++ script.js
@@ -13,6 +13,8 @@ $(function () {
           var image = new Image()
           image.src = this.result
           $('.preview').append('<div class="single-image-preview"><img src="' + image.src + '"/><label for="add-file" class="change-image">Zmień zdjęcie</label><div class="remove-image">Usuń zdjęcie</div></div>')
+          $('[data-label=exchange]').remove()
+
           //  $('[data-id=exchange]').remove()
           //   console.log("image", image)
           //   console.log("this", this)
@@ -31,8 +33,8 @@ $(function () {
   $(document).on('click', '.change-image', function () {
     $(this).parent().attr('data-label', 'exchange')
   })
-  $(document).on('change', '#add-file', function () {
-     console.log('co jest w add-file', ($('#add-file').files))
-    $('[data-label=exchange]').remove()
-  })
+  // $(document).on('change', '#add-file', function () {
+  //    console.log('co jest w add-file', ($('#add-file').files))
+  //   $('[data-label=exchange]').remove()
+  // })
 })
